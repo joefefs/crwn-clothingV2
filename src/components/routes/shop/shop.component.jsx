66f1f62cc77a28e03b0e83896bf13a1f2542a72e
josefefs <design.joseferrer@gmail.com>
2022-03-26 23:51:24 -0600

    -

--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -1,14 +1,14 @@
-import { Routes, Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
-import Category from '../category/category.component';
-import CategoriesPreview from '../categories-preview/categories-preview.component'
+import Category from "../category/category.component";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import { CategoriesProvider } from "../../../context/categories.context";
 function Shop() {
   return (
-      <Routes>
-          <Route index element={<CategoriesPreview />} />
-          <Route path=":category" element={<Category />} />
-      </Routes>
-   
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   );
 }
 // The Routes are nested within app.js so every Route generated in here, follows the path of the original route (/shop/newRoute)
