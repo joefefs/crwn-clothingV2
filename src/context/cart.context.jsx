13669e0b2530f854cf42a6a610c8c89b6d3a4ec8
josefefs <design.joseferrer@gmail.com>
2022-03-26 22:55:51 -0600

    change contexts code to use reducers for scalability. Added reducers in userContext and CartContext

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,4 +1,5 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useReducer } from "react";
+import { createAction } from "../utils/reducer/reducer.utils";
 
 // helper function to check if product is in cart
 
@@ -41,8 +42,6 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
 };
 
 const clearCartItem = (cartItems, cartItemToClear) => {
-
-
   return cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
 };
 
@@ -54,39 +53,99 @@ export const CartContext = createContext({
   removeItemFromCart: () => {},
   clearFromCart: () => {},
   cartCount: 0,
-  cartTotal: 0
+  cartTotal: 0,
 });
 
+const CART_ACTION_TYPES = {
+  SET_CART_ITEMS: "SET_CART_ITEMS",
+  SET_IS_CART_OPEN: "SET_IS_CART_OPEN",
+};
+
+const INITIAL_STATE = {
+  isCartOpen: false,
+  cartItems: [],
+  cartCount: 0,
+  cartTotal: 0,
+};
+
+const cartReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case CART_ACTION_TYPES.SET_CART_ITEMS:
+      return {
+        ...state,
+        ...payload,
+      };
+    case CART_ACTION_TYPES.SET_IS_CART_OPEN:
+      return {
+        ...state,
+        isCartOpen: payload,
+      };
+
+    default:
+      throw new Error(`Unhandled type of ${type} in cartReducer`);
+  }
+};
+
 export const CartProvider = ({ children }) => {
-  const [isCartOpen, setisCartOpen] = useState(false);
-  const [cartItems, setCartItems] = useState([]);
-  const [cartCount, setCartCount] = useState(0);
-  const [cartTotal, setCartTotal] = useState(0);
+  const [{ cartItems, isCartOpen, cartCount, cartTotal }, dispatch] =
+    useReducer(cartReducer, INITIAL_STATE);
+  // OLD USE STATE CODE!
+  //   const [isCartOpen, setisCartOpen] = useState(false);
+  //   const [cartItems, setCartItems] = useState([]);
+  //   const [cartCount, setCartCount] = useState(0);
+  //   const [cartTotal, setCartTotal] = useState(0);
 
+  //   useEffect(() => {
+  //     const newCartCount = cartItems.reduce(
+  //       (total, cartItem) => total + cartItem.quantity,
+  //       0
+  //     );
+  //     setCartCount(newCartCount);
+  //   }, [cartItems]);
 
-  useEffect(() => {
-    const newCartCount = cartItems.reduce(
+  //   useEffect(()=>{
+  //     const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price,0)
+  //     setCartTotal(newCartTotal)
+  //   },[cartItems])
+
+  //new function to dispatch payload using cartReducer
+  const updateCartItemsReducer = (newCartItems) => {
+
+    const newCartCount =  newCartItems.reduce(
       (total, cartItem) => total + cartItem.quantity,
       0
+    ); //used to live in a useEffect
+    const newCartTotal = newCartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity * cartItem.price,
+      0
+    ); //used to live in a useEffect
+
+    dispatch(
+      createAction(CART_ACTION_TYPES.SET_CART_ITEMS, {
+        cartItems: newCartItems,
+        cartTotal: newCartTotal,
+        cartCount: newCartCount,
+      })
     );
-    setCartCount(newCartCount);
-  }, [cartItems]);
-
-  useEffect(()=>{
-    const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price,0)
-    setCartTotal(newCartTotal)
-  },[cartItems])
-
+  };
   //function to add Items to cart. setCatrtItems calls the function (defined above) addCartItem, which checks if it should add the product (if it is not already there)
   const addItemToCart = (product) => {
-    setCartItems(addCartItem(cartItems, product));
+    const newCartItems = addCartItem(cartItems, product); //changed setCartItems to a variable (not using useState anymore!)
+    updateCartItemsReducer(newCartItems);
   };
 
   const removeItemFromCart = (carItemToRemove) => {
-    setCartItems(removeCartItem(cartItems, carItemToRemove));
+    const newCartItems = removeCartItem(cartItems, carItemToRemove); //changed setCartItems to a variable (not using useState anymore!)
+    updateCartItemsReducer(newCartItems);
   };
   const clearFromCart = (cartItemToClear) => {
-    setCartItems(clearCartItem(cartItems, cartItemToClear));
+    const newCartItems = clearCartItem(cartItems, cartItemToClear); //changed setCartItems to a variable (not using useState anymore!)
+    updateCartItemsReducer(newCartItems);
+  };
+  //new method for setting isCartOpen without useState:
+  const setisCartOpen = (bool) => {
+    dispatch(createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, bool));
   };
 
   const value = {
@@ -97,7 +156,7 @@ export const CartProvider = ({ children }) => {
     cartCount,
     removeItemFromCart,
     clearFromCart,
-    cartTotal
+    cartTotal,
   };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import {onAuthStateChangedListener, createUserDocumentFromAuth} from '../utils/firebase/firebase.utils'
+import { createAction } from "../utils/reducer/reducer.utils";
 
 //as  the actual value you want to access
 export const UserContext = createContext({
@@ -7,9 +8,47 @@ export const UserContext = createContext({
     setCurrentUser: () => null
 })
 
+//We create action types for the reducer function below 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+//To change the original useState for a useReducer we need to create a reducer helper function:
+const userReducer = (state, action) => {
+    console.log('dispatched!')
+    console.log(action)
+    const { type, payload } = action
+    switch(type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER :
+            return {
+                ...state,
+                currentUser: payload
+            }
+        default:
+            throw new Error (`Unhandled type ${type} in userReducer`)
+    }
+
+
+}
+
+// We define initial state for useReducer Hook
+const INITIAL_STATE = {
+    currentUser: null
+}
+
 //This provider is allowing any of it child components (We're wrappimg <App /> so every component has access)
 export const UserProvider = ({children}) => {
-    const [ currentUser, setCurrentUser ] = useState(null)
+    // const [ currentUser, setCurrentUser ] = useState(null) |||||| replaced useState Hook for useReducer Hook.
+    
+    const [state, dispatch] = useReducer(userReducer, INITIAL_STATE) //useReducer hook receives a reducer function and the initial state AND it return (destructured) a state object an a dispatch function!
+    
+    const {currentUser} = state //just destructured currentUser from state 
+    console.log(currentUser)
+
+    const setCurrentUser = ( user ) => {
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user)) //the setter function from useState won't work anymore so we have to declare it. Inside, we are dispatching the action with a payload to modify state.
+    }
+
     const value = { currentUser, setCurrentUser }
     
 
